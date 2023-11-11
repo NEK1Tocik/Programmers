aab57042e0bcafdf492f0d24c38d5c28c57a5f16
NEK1Tocik <gold81963@gmail.com>
2023-11-11 14:19:58 +0300

    SQLite3

--- lessons/main.py
+++ lessons/main.py
@@ -1,21 +1,13 @@
 import sqlite3
 
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('MyDB.db')
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS text_data (word TEXT)''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS number_data (number INTEGER)''')
-a = ["python", 102, "god", 33333, "food", "nice", "cool", 1, 25]
-for i in a:
-    if isinstance(i, str): # Если элемент списка является строкой
-        cursor.execute("INSERT INTO text_data (word) VALUES (?)", (i,))
-        word_length = len(i)
-        cursor.execute("INSERT INTO number_data (number) VALUES (?)", (word_length,))
-    else: # Если элемент списка является числом
-        if i % 2 == 0: # Если число чётное
-            cursor.execute("INSERT INTO number_data (number) VALUES (?)", (i,))
-        else: # Если число нечетное
-            cursor.execute("INSERT INTO text_data (word) VALUES ('нечётное')")
-
+# Создаем таблицу
+cursor.execute('''CREATE TABLE IF NOT EXISTS table_1 (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+# Добавляем три записи в таблицу
+cursor.execute('''INSERT INTO table_1 (name, age) VALUES ('Игорь', 19)''')
+cursor.execute('''INSERT INTO table_1 (name, age) VALUES ('Петр', 25)''')
+cursor.execute('''INSERT INTO table_1 (name, age) VALUES ('Мария', 36)''')
 # Сохраняем изменения
 conn.commit()
 # Закрываем соединение с базой данных
